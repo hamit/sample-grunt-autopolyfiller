0be05534c7822823c51e3b71e36d03b60138e76e
hamitogluh <hamitoglu.hamit@gmail.com>
2015-02-20 11:59:45 +0200

    js_to_be_polyfilled.js
    This javascript file will be polyfilled by grunt-autopolyfiller.

---
+++ js_to_be_polyfilled.js
@@ -0,0 +1,9 @@
+Object.assign.bind(Object, {}, {});
+"".trim();
+function timeout(time) {
+    return new Promise(function (fulfill) {
+        setTimeout(fulfill, time);
+    });
+}
+[{a: 1}, {b: 2}, {c: 3}].map(Object.keys);
+window['atob'].bind(null, this.btoa.call(null, '1'))();
